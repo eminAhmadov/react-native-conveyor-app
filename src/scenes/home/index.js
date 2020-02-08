7b6826c5dbd259ca7390f4e1c86a55e3eac71cdd
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-02-08 14:56:33 +0100

    Added Header Main and demo TravelPromo components to the Home Screen

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import TravelPromo from '../../components/organisms/travel-promo';
+import {View} from 'native-base';
+import HeaderMain from '../../components/organisms/header-main';
 
 const HomeScreen = ({navigation}) => (
-  <SafeAreaView>
-    <Text>Screen: Home</Text>
-  </SafeAreaView>
+  <View style={{height: '100%', width: '100%'}}>
+    <HeaderMain />
+    <TravelPromo />
+    <TravelPromo />
+    <TravelPromo />
+  </View>
 );
 
 export default HomeScreen;
