b51d62e26fde232aa3a03fb53d0e1a3ce1e077a9
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-02-08 14:54:44 +0100

    Created base of the Login Screen

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -1,14 +1,77 @@
-import React from 'react';
-import {SafeAreaView, Text, TouchableHighlight} from 'react-native';
+import React, {Component} from 'react';
+import {View, Image} from 'react-native';
+import {Container, Content, Item, Input, Button, Text} from 'native-base';
 
-const LoginScreen = ({navigation}) => (
-  <SafeAreaView>
-    <Text>Screen: Login</Text>
+const logoImage = require('../../assets/images/logo.png');
 
-    <TouchableHighlight onPress={() => navigation.navigate('Home')}>
-      <Text>Go to home</Text>
-    </TouchableHighlight>
-  </SafeAreaView>
-);
+class LoginScreen extends Component {
+  onLoginButtonPressed = () => {
+    const {navigation} = this.props;
+
+    navigation.navigate('Home');
+  };
+
+  render() {
+    const {navigation} = this.props;
+    return (
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            height: '60%',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <Image
+            style={{width: '50%'}}
+            resizeMode="contain"
+            source={logoImage}
+          />
+          <View>
+            <Item style={{width: '80%'}}>
+              <Input placeholder="Email" />
+            </Item>
+            <Item style={{width: '80%'}}>
+              <Input placeholder="Password" />
+            </Item>
+          </View>
+          <View style={{width: '100%', alignItems: 'center'}}>
+            <Button
+              rounded
+              style={{
+                borderRadius: 20,
+                width: '50%',
+                justifyContent: 'center',
+                backgroundColor: 'orange',
+              }}
+              onPress={this.onLoginButtonPressed}>
+              <Text
+                style={{textAlign: 'center', fontWeight: 'bold', fontSize: 20}}>
+                Login
+              </Text>
+            </Button>
+            <Button
+              transparent
+              onPress={() => navigation.navigate('Registration')}
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+                height: 35,
+                marginTop: 10,
+              }}>
+              <Text style={{color: 'black'}}>Registration</Text>
+            </Button>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default LoginScreen;
