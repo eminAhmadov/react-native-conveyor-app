1ef1eb193a62ac46016e3c67b43c0d0345ff8de8
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-02-09 17:28:17 +0100

    Separated Post Travel component

---
+++ src/components/organisms/post-travel.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import {Button, Item, Input} from 'native-base';
+import {scaleSize} from '../../styles/mixins';
+
+const userImage = require('../../assets/images/user.png');
+
+const PostTravel = ({editable = true}) => (
+  <View
+    style={{
+      height: '100%',
+      width: '90%',
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    <View
+      style={{
+        height: scaleSize(320),
+        width: '100%',
+        padding: '3%',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        marginBottom: 40,
+        borderColor: '#008bdb',
+        borderWidth: 4,
+        borderRadius: 20,
+      }}>
+      <View
+        style={{
+          height: '40%',
+          width: '100%',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Image
+          style={{height: '80%', width: '40%'}}
+          resizeMode="contain"
+          source={userImage}
+        />
+        <Text style={{fontSize: 20, fontWeight: '600'}}>Emin Ahmadov</Text>
+      </View>
+      {editable ? (
+        <View
+          style={{
+            height: '60%',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <Item style={{width: '80%'}}>
+            <Input placeholder="From" />
+          </Item>
+          <Item style={{width: '80%'}}>
+            <Input placeholder="To" />
+          </Item>
+          <Item style={{width: '80%'}}>
+            <Input placeholder="Date" />
+          </Item>
+          <Item style={{width: '80%'}}>
+            <Input placeholder="Comment" />
+          </Item>
+        </View>
+      ) : (
+        <Text>Not Editable</Text>
+      )}
+    </View>
+    <View
+      style={{
+        height: '15%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Button
+        rounded
+        style={{
+          borderRadius: 20,
+          width: '50%',
+          justifyContent: 'center',
+          backgroundColor: 'orange',
+        }}
+        onPress={() => {}}>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: 20,
+            color: 'white',
+          }}>
+          Post
+        </Text>
+      </Button>
+    </View>
+  </View>
+);
+
+export default PostTravel;
--- src/scenes/post/index.js
+++ src/scenes/post/index.js
@@ -1,97 +1,12 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
-import {Container, Content, Item, Input, Button} from 'native-base';
+import {View} from 'react-native';
 import HeaderMain from '../../components/organisms/header-main';
-import {scaleSize} from '../../styles/mixins';
-
-const userImage = require('../../assets/images/user.png');
+import PostTravel from '../../components/organisms/post-travel';
 
 const PostScreen = () => (
   <View style={{height: '100%', width: '100%', backgroundColor: '#45beff'}}>
     <HeaderMain />
-    <View
-      style={{
-        height: '100%',
-        width: '90%',
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <View
-        style={{
-          height: scaleSize(320),
-          width: '100%',
-          padding: '3%',
-          justifyContent: 'center',
-          backgroundColor: 'white',
-          marginBottom: 40,
-          borderColor: '#008bdb',
-          borderWidth: 4,
-          borderRadius: 20,
-        }}>
-        <View
-          style={{
-            height: '40%',
-            width: '100%',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}>
-          <Image
-            style={{height: '80%', width: '40%'}}
-            resizeMode="contain"
-            source={userImage}
-          />
-          <Text style={{fontSize: 20, fontWeight: '600'}}>Emin Ahmadov</Text>
-        </View>
-        <View
-          style={{
-            height: '60%',
-            width: '100%',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}>
-          <Item style={{width: '80%'}}>
-            <Input placeholder="From" />
-          </Item>
-          <Item style={{width: '80%'}}>
-            <Input placeholder="To" />
-          </Item>
-          <Item style={{width: '80%'}}>
-            <Input placeholder="Date" />
-          </Item>
-          <Item style={{width: '80%'}}>
-            <Input placeholder="Comment" />
-          </Item>
-        </View>
-      </View>
-      <View
-        style={{
-          height: '15%',
-          width: '100%',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Button
-          rounded
-          style={{
-            borderRadius: 20,
-            width: '50%',
-            justifyContent: 'center',
-            backgroundColor: 'orange',
-          }}
-          onPress={() => {}}>
-          <Text
-            style={{
-              textAlign: 'center',
-              fontWeight: 'bold',
-              fontSize: 20,
-              color: 'white',
-            }}>
-            Post
-          </Text>
-        </Button>
-      </View>
-    </View>
+    <PostTravel />
   </View>
 );
 
