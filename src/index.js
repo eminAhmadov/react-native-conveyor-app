26600c4742996b37e59fe2247fdc12b508811b05
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-02-09 16:00:04 +0100

    Fixed status bar color for stack navigator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import {View, StatusBar} from 'react-native';
 import Navigator from './navigations';
 
-const App = () => <Navigator />;
+const App = () => (
+  <View style={{flex: 1}}>
+    <StatusBar backgroundColor="white" barStyle="dark-content" />
+    <Navigator
+      initialRoute={{statusBarHidden: false}}
+      renderScene={() => (
+        <View>
+          <StatusBar />
+          ...
+        </View>
+      )}
+    />
+  </View>
+);
 export default App;
